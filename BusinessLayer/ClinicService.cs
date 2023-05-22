9aeed2354227a6fe013ef83795526bac05d8fe28
Mirko <mirkoscat92@gmail.com>
2023-05-22 22:36:56 +0200

    add clinicservice class

---
+++ BusinessLayer/ClinicService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+	public class ClinicService : IClinicService
+	{
+		public bool CreateClinicAnimal()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool DeleteClinicAnimal()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ClinicAnimal GetClinicAnimalById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ClinicAnimal> GetClinicAnimals()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ClinicVisit> GetClinicVisitsById(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
